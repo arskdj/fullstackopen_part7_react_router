2c1da8022cfc35e9d096a8ff1a03cf955350f3cd
arskdj <68353557+arskdj@users.noreply.github.com>
2020-09-02 22:26:42 +0300

    7.5

--- routed-anecdotes/src/App.js
+++ routed-anecdotes/src/App.js
@@ -74,6 +74,13 @@ const CreateNew = (props) => {
 
     }
 
+    const handleReset = (e) => {
+        e.preventDefault()
+        content.reset()    
+        author.reset()    
+        info.reset()    
+    }
+
     return (
         <div>
             <h2>create a new anecdote</h2>
@@ -91,6 +98,7 @@ const CreateNew = (props) => {
                     <input {...info}/>
                 </div>
                 <button>create</button>
+                <button onClick={handleReset}>reset</button>
             </form>
         </div>
     )
